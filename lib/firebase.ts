e1c0f6c5e740443df29f696de99547c21e7b1ff3
Your Namepiciosaasd <you@example.com>
2025-04-29 23:40:04 +0300

    ✅ Fix final firebase.ts fără conflicte

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,20 +1,3 @@
-import { initializeApp } from firebaseapp;
-import { getAuth } from firebaseauth;
-import { getFirestore } from firebasefirestore;
-import { getStorage } from firebasestorage;
-
-const firebaseConfig = {
-  apiKey process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
-};
-
-const app = initializeApp(firebaseConfig);
-
-=======
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -30,7 +13,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
->>>>>>> 604c1e1869baaed54f8f3a2a5872f73b8e8a4497
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
